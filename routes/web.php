51357c6a5fc27b1ea267c4833c118d261c7a55b7
jusrusswebb <122849382+jusrusswebb@users.noreply.github.com>
2024-09-21 15:34:11 -0700

    grouped/removed routes

--- routes/web.php
+++ routes/web.php
@@ -12,24 +12,21 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/add-client', [ClientController::class, 'create'])->name('add-client');
-Route::post('/add-client', [ClientController::class, 'store']);
-
-Route::get('/add-service', [ServiceController::class, 'create'])->name('add-service');
-Route::post('/add-service', [ServiceController::class, 'store'])->name('store-service');
-
-Route::delete('/service/{id}', [ServiceController::class, 'destroy'])->name('delete-service');
-
-Route::delete('/client/{id}', [ClientController::class, 'destroy'])->name('delete-client');
-
-Route::get('/client-dashboard', [ClientDashboardController::class, 'index'])->name('client-dashboard');
-
 Route::middleware('auth')->group(function () {
-    Route::get('/invoices/create', [InvoiceController::class, 'create'])->name('invoices.create');
-    Route::post('/invoices', [InvoiceController::class, 'store'])->name('invoices.store');
-    Route::get('/invoices/{invoice}', [InvoiceController::class, 'show'])->name('invoices.show');
+    Route::get('/add-client', [ClientController::class, 'create'])->name('add-client');
+    Route::post('/add-client', [ClientController::class, 'store']);
+
+    Route::get('/add-service', [ServiceController::class, 'create'])->name('add-service');
+    Route::post('/add-service', [ServiceController::class, 'store'])->name('store-service');
+
+    Route::delete('/service/{id}', [ServiceController::class, 'destroy'])->name('delete-service');
+
+    Route::delete('/client/{id}', [ClientController::class, 'destroy'])->name('delete-client');
+
+    Route::get('/client-dashboard', [ClientDashboardController::class, 'index'])->name('client-dashboard');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
 
 
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
